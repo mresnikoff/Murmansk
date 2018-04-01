cbf690790a35c90b57bd3b5cb890b5d97f206cf1
mresnikoff <35782180+mresnikoff@users.noreply.github.com>
2018-04-01 13:46:55 -0400

    Add files via upload

---
+++ Assignment_4/bootstrap/css/studentAdded.php
@@ -0,0 +1,58 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>Assignment 04 - Contrasting Team Member</title>
+
+	<!-- Custom Style Sheets -->
+	<link href="https://fonts.googleapis.com/css?family=Cinzel|Lato" rel="stylesheet">
+	<!-- link to style.css -->
+	<link rel="stylesheet" type="text/css" href="./bootstrap/css/override.css">
+</head>
+
+	
+
+
+
+	<body>
+
+		<header>
+
+
+
+		</header>
+
+			<div class="container">
+
+				
+
+				Success! The query did not error out
+
+
+			<section>
+
+				
+
+				<h2>Thank You!</h2>
+
+
+
+				<p>To go back, click:</p>
+
+				<a class="Home Page" href = "index.php">Here</a>
+
+
+
+			</section>
+
+
+
+	</div>
+
+	</body>
+
+</html>
---
+++ Assignment_4/studentAdded.php
@@ -0,0 +1,57 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>Assignment 04</title>
+
+	<!-- Custom Style Sheets -->
+	<link href="https://fonts.googleapis.com/css?family=Cinzel|Lato" rel="stylesheet">
+	<!-- link to style.css -->
+	<link rel="stylesheet" type="text/css" href="./bootstrap/css/override.css">
+</head>
+
+	
+
+
+
+	<body>
+
+		<header>
+
+
+
+		</header>
+
+			<div class="container">
+
+				
+
+				Success! The query worked!
+
+
+			<section>
+
+				
+
+				<h2>Thanks!</h2>
+
+
+
+				<p>To go back, click:</p>
+<a class="Home Page" href = "index.php">Here</a>
+
+
+
+			</section>
+
+
+
+	</div>
+
+	</body>
+
+</html>
